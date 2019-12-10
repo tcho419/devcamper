502a59b789eae7d09d8e341cdf7033162f68aa65
Timothy Ho <tho@cra.com>
2019-12-10 17:27:26 -0500

    add routes for 'bootcamp', reorganize by using express router and controller methods

---
+++ routes/bootcamps.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp
+} = require('../controllers/bootcamps'); // destructure out the controller methods
+
+const router = express.Router();
+
+// organize the routes by endpoint and type
+router
+  .route('/')
+  .get(getBootcamps)
+  .post(createBootcamp);
+
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+/*
+app.get('/', (req, res) => {
+  //res.send('Hello from express'); // This sends a simple text message or HTML
+  // res.json sends JSON and can also do res.sendStatus
+
+  // good way is to manually chain repsonse and status
+  res.status(200).json({ success: true, data: { id: 1 }});
+});
+*/
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+// Set Port number and listen
 const PORT = process.env.PORT || 5000;
 
 app.listen(
